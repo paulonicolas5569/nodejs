fd62dce4618182987ee4a2b02e4d870ced596c38
Paulo Nicolas <115593922+paulonicolas5569@users.noreply.github.com>
2023-08-11 22:03:51 -0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,45 @@
+/**
+ * app para calcular a média de 2 notas 
+ *
+ */
+
+//impoprtação do pacote readline-sync
+const read = require("readline-sync")
+
+//Vareaveis
+
+let name, disciplina
+let nota1, nota2, media 
+
+console.clear()
+
+console.log("'########:::'#######::'##:::::::'########:'########:'####:'##::: ##:")
+console.log("##.... ##:'##.... ##: ##::::::: ##.....::... ##..::. ##:: ###:: ##:")
+console.log("##:::: ##: ##:::: ##: ##::::::: ##:::::::::: ##::::: ##:: ####: ##:")
+console.log("########:: ##:::: ##: ##::::::: ######:::::: ##::::: ##:: ## ## ##:")
+console.log("##.... ##: ##:::: ##: ##::::::: ##...::::::: ##::::: ##:: ##. ####:")
+console.log("##:::: ##: ##:::: ##: ##::::::: ##:::::::::: ##::::: ##:: ##:. ###:")
+console.log("########::. #######:: ########: ########:::: ##::::'####: ##::. ##:")
+console.log("........::::.......:::........::........:::::..:::::....::..::::..:: ")           
+
+//entrada das vareaveis 
+nome = read.question("digite o seu nome: ")
+disciplina = read.question("digite a diciplina: ")
+nota1 = Number(read.question("digite a nota1: "))
+nota2 = Number(read.question("digite a nota2: "))
+
+//processamento
+media = (nota1 + nota2 ) / 2
+
+//saida
+console.clear()
+console.log("----------------------------")
+console.log("ficha do aluno") 
+console.log(`nome: ${nome}`)
+console.log(`disciplina: ${disciplina}`)
+console.log(`nota1: ${nota1}`)
+console.log(`nota2: ${nota2}`)
+console.log(`media final: ${media}`)
+console.log("-----------------------------")
+
+
