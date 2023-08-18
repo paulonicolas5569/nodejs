c902c2463ab1306a68985a9b39f065bf733d030e
Paulo Nicolas <115593922+paulonicolas5569@users.noreply.github.com>
2023-08-18 20:04:17 -0300

    Add files via upload

---
+++ exercicios/conversordepeso.js
@@ -0,0 +1,26 @@
+/**
+ * conversor de temperatura 
+ * fahrenheit para celsius
+ */
+
+//importante do módulo (readline-sync)
+const read = require("readline-sync")
+//variáveis
+let libras , kilos
+console.clear()
+console.log("---------------------------")
+console.log("-- conversor de peso")
+console.log("--libras para kilos")
+console.log("")
+//entrada 
+console.clear()
+//entrada
+console.log("bem vindo ao conversor de peso")
+lib = Number(read.question("digite a peso em libras:"))
+//processamento
+kil = (lib / 2.2 ) 
+console.log
+//saida 
+console.log("o peso em  " + lib + " em kil é " + kil +"")
+
+
