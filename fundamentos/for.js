16a853a0e18354387aa65c2a0f5dec927de11757
Paulo Nicolas <115593922+paulonicolas5569@users.noreply.github.com>
2023-09-18 21:59:47 -0300

    Add files via upload

---
+++ fundamentos/for.js
@@ -0,0 +1,15 @@
+/**
+ * estudo do laço for 
+ */
+
+console.clear()
+for (let i = 1 ; i < 11 ; i++ ) {
+   console.log(i) 
+}
+ console.log(" fim do laço for ")
+
+ for (let j = 10 ; j > 0 ; j-- ) {
+    console.log(j) 
+ }
+ console.log(" fim do laço for ")
+ console.log (" BUUUUMMMMM!!!!!!!")
