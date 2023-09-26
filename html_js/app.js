c1d57b8c919e86cc9bb2749dbca4263242cd9c3b
Paulo Nicolas <115593922+paulonicolas5569@users.noreply.github.com>
2023-09-25 21:54:36 -0300

    Add files via upload

---
+++ html_js/app.js
@@ -0,0 +1,10 @@
+/**.
+ * Exemplo de link de um documento js com html 
+ */
+
+function helloJS(){
+    console.log("Hello JS")
+    let nome 
+    nome = prompt("qual é o seu nome  ")
+    alert(`Hello ${nome}`) 
+}
